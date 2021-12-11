0fab4b288ec34c9c6def338f2a55eccce447699c
ahmad irfan <ahmadrfn9@gmail.com>
2021-12-11 13:53:22 +0800

    myorders

--- resources/views/order/order.blade.php
+++ resources/views/order/order.blade.php
@@ -1,20 +1,22 @@
 <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
 <x-app-layout>
-<x-slot name="header">
+    <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Orders
+            Order Tracker
         </h2>
     </x-slot>
 
-    <head>
+    <!-- <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-    </head>
+    </head> -->
 
-    <body>
-        <h2>Hello world</h2>
-    </body>
+    <div class="container mx-auto">
+        <div class="max-w-6xl mx-auto py-10 sm:px-6 lg:px-8 ">
+            <h2 class="font-sans">Hello world</h2>
+        </div>
+    </div>
 </x-app-layout>
--- storage/framework/views/03895bf73d292ac8b2057bbd012a66feabbe816d.php
+++ storage/framework/views/03895bf73d292ac8b2057bbd012a66feabbe816d.php
@@ -6,22 +6,37 @@
 <?php if ($component->shouldRender()): ?>
 <?php $__env->startComponent($component->resolveView(), $component->data()); ?>
 <?php $component->withAttributes([]); ?>
- <?php $__env->slot('header', null, []); ?> 
+     <?php $__env->slot('header', null, []); ?> 
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Orders
+            Order Tracker
         </h2>
      <?php $__env->endSlot(); ?>
 
-    <head>
+    <!-- <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-    </head>
+    </head> -->
 
-    <body>
-        <h2>Hello world</h2>
-    </body>
+    
+    <!-- <div class="max-w-6xl mx-auto py-10 sm:px-6 lg:px-8">
+        <div class="flex flex-col">
+            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                    <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                        <h2>Hello world</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> -->
+
+    <div class="container mx-auto">
+        <div class="max-w-6xl mx-auto py-10 sm:px-6 lg:px-8 ">
+            <h2 class="font-sans">Hello world</h2>
+        </div>
+    </div>
  <?php echo $__env->renderComponent(); ?>
 <?php endif; ?>
 <?php if (isset($__componentOriginal8e2ce59650f81721f93fef32250174d77c3531da)): ?>
