291e023cd5b44cc6972b6fdb3a0373969af7f7e4
ahmad irfan <ahmadrfn9@gmail.com>
2021-12-11 19:01:35 +0800

    myorders

--- resources/views/order/order.blade.php
+++ resources/views/order/order.blade.php
@@ -3,7 +3,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Order Tracker
+            Orders List
         </h2>
     </x-slot>
 
@@ -15,8 +15,20 @@
     </head> -->
 
     <div class="container mx-auto">
-        <div class="max-w-6xl mx-auto py-10 sm:px-6 lg:px-8 ">
-            <h2 class="font-sans">Hello world</h2>
+        <div class="max-w-6xl mx-auto py-10 sm:px-6 lg:px-8">
+            <div class="shadow overflow-hidden border-b border-gray-200 py-3 px-3 text-lg font-semibold bg-gray-50">
+                <h2>Active orders</h2>
+            </div>
+            <div class="shadow overflow-hidden border-b border-gray-200 py-3 px-3 bg-white">
+                <a href="/tracker.blade.php">Tracking order example (id)</a><br>
+                <i>List active orders (menu name, qty, price)</i>
+            </div>
+            <div class="shadow overflow-hidden border-b border-gray-200 py-3 px-3 text-lg font-semibold bg-gray-50">
+                <h2>Past orders</h2>
+            </div>
+            <div class="shadow overflow-hidden border-b border-gray-200 py-3 px-3 bg-white">
+                <i>List past orders (menu name, qty, price)</i>
+            </div>
         </div>
     </div>
 </x-app-layout>
