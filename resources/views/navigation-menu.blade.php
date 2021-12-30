39729cbd2936105feb0a6969c09ed7ba1898f1c2
ZulhusniAzhar <92666477+ZulhusniAzhar@users.noreply.github.com>
2021-12-30 10:49:39 +0800

    profile pic

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -36,8 +36,8 @@
                 <!--<div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('order.index') }}" :active="request()->routeIs('order.index')">
                         {{ __('Review') }}
-                    </x-jet-nav-link>-->
-                </div>
+                    </x-jet-nav-link>
+                </div>-->
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
